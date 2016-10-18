8809bea46666d230c4dcc19001fe8b0869c57627
Bernat Orellana <borellan@xtec.cat>
2016-10-18 20:00:12 +0200

    Exemple de ListView

---
+++ UF1/20161013/ListBoxes/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ListBoxes
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            ObservableCollection<Provincia> provs = new ObservableCollection<Provincia>();       
+            provs.Add(new Provincia(1, "Barcelona", 2250000, 300, "Futbol Club"));
+            provs.Add(new Provincia(2, "Tarrragona", 500000, 200, "Tarragona m'esborrona"));
+            provs.Add(new Provincia(3, "Girona", 250000, 300, "Eooooooo!"));
+            provs.Add(new Provincia(4, "Lleida", 250000, 300, "From la terra ferma!"));
+
+            listView.ItemsSource = provs;
+            //
+
+            //listView.DisplayMemberPath = "Nom";
+
+
+
+        }
+    }
+}
---
+++ UF1/20161013/ListBoxes/Provincia.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListBoxes
+{
+    class Provincia : INotifyPropertyChanged
+    {
+
+        // Event per declarar que hi ha hagut canvis en les propietats
+        // de la classe.
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+
+        public Provincia(int pCodi, string pNom, int pPoblacio, int pSuperficie, string pDesc)
+        {
+            Codi = pCodi;
+            Nom = pNom;
+            Superficie = pSuperficie;
+            Poblacio = pPoblacio;
+            Desc = pDesc;
+
+        }
+
+        //----------------------------
+
+        private int mCodi;
+   
+        public int Codi
+        {
+            get { return mCodi; }
+            set {
+                if (value < 0)
+                    throw new Exception("PUFFFFF");
+                mCodi = value; }
+        }
+
+        private string mNom;
+
+        public string Nom
+        {
+            get { return mNom; }
+            set {
+                mNom = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+
+        private int mPoblacio;
+
+        public int Poblacio
+        {
+            get { return mPoblacio; }
+            set { mPoblacio = value; }
+        }
+
+ 
+        private int mSuperficie;
+
+        public int Superficie
+        {
+            get { return mSuperficie; }
+            set { mSuperficie = value; }
+        }
+
+        private string mDesc;
+
+        public string Desc
+        {
+            get { return mDesc; }
+            set { mDesc = value; }
+        }
+        /// <summary>
+        ///  Mètode per avisar a les classes de la UI que hi ha hagut
+        ///  canvis en alguna propietat, i que per tant s'han d'actualitzar.
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            // Raise the PropertyChanged event, passing the name of the property whose value has changed.
+            this.PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
