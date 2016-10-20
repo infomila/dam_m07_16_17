ea6f120cca16d37bef16c947f0410ad303884f8c
Bernat Orellana <borellan@xtec.cat>
2016-10-20 17:42:46 +0200

    Exemple de Hubs i Frames

---
+++ UF1/20161019/ExempleFrameHub/MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace ExempleFrameHub
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void btnA_Click(object sender, RoutedEventArgs e)
+        {
+            frmPrincipal.Navigate(typeof(PageA), "VALOR A");
+        }
+        private void btnB_Click(object sender, RoutedEventArgs e)
+        {
+            frmPrincipal.Navigate(typeof(PageA), "VALOR B");
+        }
+
+        private void btnBack_Click(object sender, RoutedEventArgs e)
+        {
+            if (frmPrincipal.CanGoBack)
+            {
+                frmPrincipal.GoBack();
+            }
+        }
+    }
+}
