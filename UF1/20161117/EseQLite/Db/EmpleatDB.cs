a67784561d17950c99ad148a74c0854fa6f5cdab
Bernat Orellana <borellan@xtec.cat>
2016-11-17 20:54:00 +0100

    s

---
+++ UF1/20161117/EseQLite/Db/EmpleatDB.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EseQLite.Db
+{
+    class EmpleatDB
+    {
+        public int getNumeroEmpleats()
+        {
+
+            using (EmpresaContext ctx = new EmpresaContext())
+            {
+                //Get student name of string type
+                using (var connection = ctx.Database.GetDbConnection())
+                {
+                    connection.Open();
+
+                    using (var command = connection.CreateCommand())
+                    {
+
+                    }
+                }
+            }
+            return 0;
+        }
+    }
+}
---
+++ UF1/20161117/EseQLite/Db/EmpresaContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EseQLite.Db
+{
+    class EmpresaContext : DbContext
+    {
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=empresa.db");
+        }
+    }
+}
