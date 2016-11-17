759953f572b608aaa84bf89e9905f49bc17f3027
Bernat Orellana <borellan@xtec.cat>
2016-11-17 20:16:28 +0100

    Exemple DB

---
+++ UF1/20161117/AppBD/Model/Empresa.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace AppBD.Model
+{
+    /*protected SQLiteAsyncConnection GetConnection(string dbName)
+    {
+        // dbName = 'hoge.db'	
+        // pool is acquired by ' new SQLiteConnectionPool(new   SQLitePlatformWinRT());'
+        if (null == pool) { return null; }
+        return new SQLiteAsyncConnection(() => pool.GetConnection(new SQLiteConnectionString(ApplicationData.Current.LocalFolder.Path + "\\" + dbName, false)));
+    }*/
+
+    public class EmpresaContext : DbContext
+    {
+        public DbSet<Empleat> Blogs { get; set; }
+    
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=emp.db");
+        }        
+    }
+
+    public class Empleat
+    {
+
+        //public static ObservableCollection<string> getEmpleats()
+        public static int getEmpleats()
+        {
+            using (var ctx = new EmpresaContext())
+            {
+                //Get student name of string type
+                using ( var connection  = ctx.Database.GetDbConnection())
+                {
+                    connection.Open();
+
+                    using (var command = connection.CreateCommand())
+                    {
+
+                        //command.CommandText = "insert into emp values ( (SELECT COUNT(*)+1 FROM emp), 'Bartolomeu' );";
+                        //int rowInserted = command.ExecuteNonQuery();
+
+                        command.CommandText = "SELECT * FROM emp";       
+                                         
+                    }
+                }
+             }
+            return 0;
+        }
+
+        private string mNom;
+
+        public string Nom
+        {
+            get { return mNom; }
+            set { mNom = value; }
+        }
+
+    }
+}
