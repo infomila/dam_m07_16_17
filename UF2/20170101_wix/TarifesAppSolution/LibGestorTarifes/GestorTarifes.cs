3802cbb61e810a1624d725c793584a8d7a7cca43
Bernat Orellana <borellan@xtec.cat>
2017-01-31 20:15:22 +0100

    projectes wix

---
+++ UF2/20170101_wix/TarifesAppSolution/LibGestorTarifes/GestorTarifes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibGestorTarifes
+{
+    public class GestorTarifes
+    {
+        public static Decimal getTarifa(Decimal pPreuBase)
+        {
+            Decimal descompte = 0;
+            if ( DateTime.Now>new DateTime(DateTime.Now.Year,12, 24))
+            {
+                descompte = 20;
+            }
+            return pPreuBase * (1-descompte/100);
+        }
+    }
+}
---
+++ UF2/20170101_wix/TarifesAppSolution/TarifesApp/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TarifesApp
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Producte p = Producte.getProductes()[0];
+            txtPreuBase.Text = p.PreuBase.ToString();
+            txtPreuVenda.Text = p.PreuVenda.ToString();
+        }
+    }
+}
