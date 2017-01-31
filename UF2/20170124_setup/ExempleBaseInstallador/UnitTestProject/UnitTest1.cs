123c59717c63c034569eba4613b8ceaf45601b72
Bernat Orellana <borellan@xtec.cat>
2017-01-31 20:19:52 +0100

    esborrar merda

---
+++ UF2/20170124_setup/ExempleBaseInstallador/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ExempleBaseInstallador;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Persona p = new Persona();
+            p.crida(9);
+            p.crida(7);
+        }
+    }
+}
